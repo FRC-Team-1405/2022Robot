6863f077eff730a46db74c135e81aaefd27278b0
Chris McDonald <McDonaldChristopherC@gmail.com>
2022-02-23 22:43:58 -0500

    auto mode changes fixed backup added drive to point added cargo points

--- src/main/java/frc/robot/trajectories/GenerateTrajectory.java
+++ src/main/java/frc/robot/trajectories/GenerateTrajectory.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
@@ -21,21 +22,39 @@ public class GenerateTrajectory {
     public static final double maxAcceleration = .1;
     private GenerateTrajectory(){} 
 
-    private static TrajectoryConfig config = new TrajectoryConfig(maxSpeed, maxAcceleration)
-                                                    .setKinematics(Constants.SwerveBase.KINEMATICS);
+    private static TrajectoryConfig baseConfig(){
+        return new TrajectoryConfig(maxSpeed, maxAcceleration)
+                        .setKinematics(Constants.SwerveBase.KINEMATICS);
+    }
 
     public static Trajectory backupOneMeter(Pose2d currentPosition) { 
+        TrajectoryConfig config = baseConfig().setReversed(true);
         Trajectory trajectory = TrajectoryGenerator.generateTrajectory( 
-            // Start at the origin facing the +X direction
-            new Pose2d(0, 0, new Rotation2d(0)),
-            // Pass through these two interior waypoints, making an 's' curve path
-            List.of(new Translation2d(0, Units.feetToMeters(-1)), new Translation2d(0, Units.feetToMeters(-2))),
-            // End 3 feet straight ahead of where we started, facing forward
-            new Pose2d(Units.feetToMeters(0), Units.feetToMeters(-3), new Rotation2d(0)),
+            new Pose2d( 0, 0, new Rotation2d() ),
+            List.of(),
+            new Pose2d( -1, 0, new Rotation2d() ),
             config);
-        return trajectory.relativeTo(currentPosition); 
+        return trajectory.transformBy( new Transform2d( trajectory.getInitialPose(), currentPosition )); 
     } 
 
+    public static Trajectory driveTo(Pose2d currentPosition, Pose2d targetPosition){
+        TrajectoryConfig config = baseConfig();
+        Rotation2d facing = calculateAngle(currentPosition, targetPosition);
+        Trajectory trajectory = TrajectoryGenerator.generateTrajectory( 
+            new Pose2d(currentPosition.getX(), currentPosition.getY(), facing),
+            List.of(),
+            new Pose2d(targetPosition.getX(), targetPosition.getY(), facing),
+            config);        
+        return trajectory;
+    }
+
+    private static Rotation2d calculateAngle(Pose2d p1, Pose2d p2)
+    {
+        double degrees = Math.toDegrees(Math.atan2(p2.getX() - p1.getX(), p2.getY() - p1.getY()));
+        degrees = degrees + Math.ceil( -degrees / 360 ) * 360;
+    
+        return Rotation2d.fromDegrees(degrees);
+    }
     // public static Trajectory twoBallAutoLeftPartOne(Pose2d currentPosition){ 
     //     Trajectory trajectory = TrajectoryGenerator.generateTrajectory(new Pose2d(0, 0, new Rotation2d(0)), 
     //     List.of(new Translation2d(0, Units.feetToMeters(1)), new Translation2d(0, Units.feetToMeters(2))), new Pose2d(), config)
